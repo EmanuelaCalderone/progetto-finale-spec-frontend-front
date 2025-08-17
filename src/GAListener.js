47382edd1ccecde11ac82f72ed36e7b6adcc7b5c
Emanuela Calderone <emanuelacalderone3006@gmail.com>
2025-08-17 11:27:53 +0200

    Edit GAListener.js

--- src/GAListener.js
+++ src/GAListener.js
@@ -5,7 +5,7 @@ function GAListener() {
     const location = useLocation(); // prendo l’URL attuale della pagina
 
     useEffect(() => {
-        window.gtag('config', 'G-ZFF2XWFHRP', {
+        window.gtag('config', 'G-FCY82ELJ9D', {
             page_path: location.pathname, // segnala a GA il percorso della pagina
         });
     }, [location]); // ogni volta che cambia pagina, invia il nuovo percorso
